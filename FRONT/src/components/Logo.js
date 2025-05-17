052880cdcd2f50d11bf441773b352fa80b5c423c
zoatel <yaseenashraf@protonmail.com>
2025-05-17 09:39:30 +0300

    Replaced all files with new project

--- FRONT/src/components/Logo.js
+++
@@ -1,7 +0,0 @@
-export default function Logo() {
-  return (
-    <div className="flex items-center justify-center">
-      <img src="/logo.png" alt="Logo" className="h-20 w-20 w-auto" />
-    </div>
-  );
-}
